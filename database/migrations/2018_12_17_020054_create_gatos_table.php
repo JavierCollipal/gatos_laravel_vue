f119de5429c9850ed9483ece8ccaa0e25bc4046c
Javier Collipal <jcollipal1212@gmail.com>
2018-12-16 23:45:56 -0300

    Creando Modelo parte dos/ migrate completo

---
+++ database/migrations/2018_12_17_020054_create_gatos_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGatosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gatos', function (Blueprint $table) {
+            
+            $table->increments('id');
+            $table->string('nombre',200);
+  
+            $table->integer('id_raza')->unsigned();
+  
+            $table->integer('id_color')->unsigned();
+  
+            $table->integer('id_pelaje')->unsigned();
+  
+            $table->integer('id_complexion')->unsigned();
+  
+            $table->integer('id_caracter')->unsigned();
+  
+            $table->foreign('id_raza')->references('id')->on('razas')->onUpdate('cascade');
+
+            $table->foreign('id_color')->references('id')->on('colores')->onUpdate('cascade');
+            
+            $table->foreign('id_pelaje')->references('id')->on('pelajes')->onUpdate('cascade');
+            
+            $table->foreign('id_complexion')->references('id')->on('complexion')->onUpdate('cascade');
+
+            $table->foreign('id_caracter')->references('id')->on('caracter')->onUpdate('cascade');
+  
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gatos');
+    }
+}
